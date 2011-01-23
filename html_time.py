972993da923060e4349c98750043a627fd529425
hesumi <email@toshiesumi.com>
2011-01-23 06:49:42 +0000

    week2 homework submition

---
+++ html_time.py
@@ -0,0 +1,20 @@
+import time
+import datetime
+
+#print "Here is the time: %s" % time.time()
+#print "and again: %s" % datetime.datetime.now()
+
+##########  Compose html doc for current time ###########
+lst = []
+lst.append('<html>')
+lst.append('<head><title>Current Time</title></head>')
+lst.append('<body>')
+lst.append('<h1>Current Time</h1>')
+lst.append('<pre>')
+lst.append("Here is the time: %s" % time.time()) 
+lst.append("and again: %s" % datetime.datetime.now())
+lst.append('</pre>')
+lst.append('</body>')
+lst.append('</html>')
+
+print '\n'.join(lst)
